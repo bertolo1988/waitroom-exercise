dd9b00cfd15cf08188f7a8acd50826d41d8a2a3a
bertolo1988 <tiagobertolo@gmail.com>
2022-08-11 14:45:39 +0100

    Basic TS+Graphql setup.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 export const ServerConfig = {
+  NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT || 3000,
   POSTGRES_CONNECTION_STRING: process.env.POSTGRES_CONNECTION_STRING,
 };
