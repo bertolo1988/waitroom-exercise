85fc70596c254f97d7bb69897f222148d783c3d9
bertolo1988 <tiagobertolo@gmail.com>
2022-08-12 00:56:42 +0100

    Connect server to postgre.

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,30 @@ import path from 'path';
 import { ServerConfig } from './config';
 import { ApolloServer } from 'apollo-server';
 import { importSchema } from 'graphql-import';
+import { createPool } from 'slonik';
 
 import { resolvers } from './resolvers';
 
 const schemaPath = path.resolve(__dirname, './schema.graphql');
 
+const { POSTGRES_CONNECTION_STRING } = ServerConfig;
+if (!POSTGRES_CONNECTION_STRING) {
+  throw new Error('Must provide a connection string!');
+}
+
 export let graphQLServer: ApolloServer;
 
 export async function runServer() {
+  const pool = createPool(POSTGRES_CONNECTION_STRING as string, {
+    captureStackTrace: false,
+    connectionTimeout: 60 * 1_000,
+  });
+
   graphQLServer = new ApolloServer({
+    context: ({ req }) => ({
+      pool,
+      req,
+    }),
     resolvers,
     typeDefs: importSchema(schemaPath),
   });
