048181be4c3a31d96f2aeae5e0e15f98b04558ca
bertolo1988 <tiagobertolo@gmail.com>
2022-08-12 17:39:38 +0100

    Add test for getAiResponse.

--- test/Query/getAiResponse.spec.ts
+++ test/Query/getAiResponse.spec.ts
@@ -26,7 +26,6 @@ describe('getAiResponse', () => {
           body
         }
       }`,
-      // variables: { "getAiResponseId": "1" }
     };
     const response = await request(testServer.server)
       .post('/graphql')
@@ -35,6 +34,34 @@ describe('getAiResponse', () => {
     expect(response.body.errors).toBeDefined();
     expect(response.body.errors[0].message).toBe(
       'Variable "$getAiResponseId" of required type "Int!" was not provided.',
+    );
+  });
+
+  it('should id, textHash, openAiID, title and body should be defined if requested on a successfull response', async () => {
+    const mutationData = {
+      query: `query GetAiResponse($getAiResponseId: Int!) {
+        getAiResponse(id: $getAiResponseId) {
+          id
+          textHash
+          openAiID
+          title
+          body
+        }
+      }`,
+      variables: { getAiResponseId: '1' },
+    };
+    const response = await request(testServer.server)
+      .post('/graphql')
+      .send(mutationData);
+
+    expect(response.body.data.getAiResponse).toBe(
+      expect.objectContaining({
+        id: expect.any(Number),
+        textHash: expect.any(String),
+        openAiID: expect.any(String),
+        title: expect.any(String),
+        body: expect.any(String),
+      }),
     );
   });
 });
